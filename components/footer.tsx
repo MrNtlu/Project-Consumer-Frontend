32e5eb5823f5123eb49fa74d94a2c141fa0a2c32
Burak Fidan <97burakfidan97@gmail.com>
2023-09-20 16:09:11 +0300

    Icon changes. Minor QoL changes.

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,19 +11,19 @@ const Footer = () => {
             <Image className='rounded-full' src="/static/images/logo_black.png" alt='logo' width="100" height="100" />
         </a>
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 Watchlistfy —
-            <a href="https://twitter.com/BurakFNtlu" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@MrNtlu</a>
+            <a href="https://twitter.com/BurakFNtlu" className="text-gray-600 ml-1 hover:text-[#2196F3]" rel="noopener noreferrer" target="_blank">@MrNtlu</a>
         </p>
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-        <a href="https://play.google.com/store/apps/details?id=com.mrntlu.projectconsumer" className="text-gray-500">
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start ">
+        <a href="https://play.google.com/store/apps/details?id=com.mrntlu.projectconsumer" className="text-gray-500 hover:text-[#2196F3]">
             <BsGooglePlay />
         </a>
-        <a href='https://twitter.com/watchlistfy' className="ml-3 text-gray-500">
+        <a href='https://twitter.com/watchlistfy' className="ml-3 text-gray-500 hover:text-[#2196F3]">
             <BsTwitter />
         </a>
-        <a href='privacy-policy.html' className="sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-300 sm:mt-0 ml-3 text-gray-500">
+        <a href='privacy-policy.html' className="sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-300 sm:mt-0 ml-3 text-gray-500 hover:text-[#2196F3]">
             <MdPrivacyTip />
         </a>
-        <a href='terms-conditions.html' className="ml-3 text-gray-500">
+        <a href='terms-conditions.html' className="ml-3 text-gray-500 hover:text-[#2196F3]">
             <BsFillFileEarmarkTextFill />
         </a>
         </span>
--- components/hero.tsx
+++ components/hero.tsx
@@ -11,12 +11,12 @@ const Hero = () => {
             <div className="lg:flex-grow md:w-1/2 lg:pl-12 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
                 <h1 className="title-font xl:text-5xl sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Watchlistfy - Track Your Fun!</h1>
                 <p className="mb-16 leading-relaxed xl:text-lg text-base">Discover Anime, Movies, TV Shows, Games - all in one app! Track & list your fun.</p>
-                <div className="flex lg:flex-row md:flex-col">
+                <div className="flex lg:flex-row md:flex-col hover:text-[#2196F3]">
                     <a href="https://play.google.com/store/apps/details?id=com.mrntlu.projectconsumer">
-                        <button className="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
+                        <button className="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center focus:outline-none">
                             <BsGooglePlay size={25} />
                             <span className="ml-4 flex items-start flex-col leading-none">
-                                <span className="text-xs text-gray-600 mb-1">GET IT ON</span>
+                                <span className="text-xs mb-1">GET IT ON</span>
                                 <span className="title-font font-medium">Google Play</span>
                             </span>
                         </button>

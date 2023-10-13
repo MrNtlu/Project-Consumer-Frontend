0210fd2f33f88ec65f11d59932195e38808ec979
Burak Fidan <97burakfidan97@gmail.com>
2023-10-13 14:45:39 +0300

    Minor text change.

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -31,7 +31,7 @@ const Pricing = () => {
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" className="w-3 h-3" viewBox="0 0 24 24">
                             <path d="M20 6L9 17l-5-5"></path>
                         </svg>
-                        </span>User List limited to 125
+                        </span>User List limited to 175
                     </p>
                     <p className="flex items-center text-gray-600 mb-6">
                         <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">

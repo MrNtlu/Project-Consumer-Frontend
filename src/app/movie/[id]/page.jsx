3125a051472ecffbcd6b6578f5baf83654305383
Burak Fidan <97burakfidan97@gmail.com>
2023-11-22 18:53:07 +0300

    Metadata added.

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import React from 'react'
+import { Banner } from "../../../../public/static/images/twitter_banner.png"
+import Head from 'next/head';
 
 const API_URL = process.env.BASE_URL;
 
@@ -16,11 +18,33 @@ export default async function MovieDetailsPage({ params }) {
     const data = await getMovieDetails(params.id);
     const movieDetails = data.data;
 
+    const movieTitle = movieDetails.title_en || movieDetails.title_original;
+    const movieDescription = movieDetails.description;
+    const movieImage = movieDetails.backdrop || movieDetails.image_url;
+
     return (
-        <div className="w-full">
+      <div className="w-full">
+        <Head>
+          <title>{movieTitle}</title>
+          <meta name="description" content={movieDescription} />
+
+          {/* Twitter metadata */}
+          <meta name="twitter:card" content={movieDetails.backdrop || Banner} />
+          <meta name="twitter:title" content={movieTitle} />
+          <meta name="twitter:description" content={movieDescription} />
+          <meta name="twitter:image" content={movieImage} />
+
+          {/* Facebook Open Graph metadata */}
+          <meta property="og:title" content={movieTitle} />
+          <meta property="og:description" content={movieDescription} />
+          <meta property="og:image" content={movieImage} />
+          <meta property="og:url" content={`${API_URL}/movie/${params.id}`} />
+          <meta property="og:type" content="website" />
+        </Head>
+
         <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
           <Image
-            src={movieDetails.backdrop || movieDetails.image_url}
+            src={movieImage}
             width={500}
             height={300}
             className="rounded-lg"
@@ -33,11 +57,11 @@ export default async function MovieDetailsPage({ params }) {
             alt="Movie poster" />
           <div className="p-2">
             <h2 className="text-lg mb-3 font-bold">
-              {movieDetails.title_en || movieDetails.title_original}
+              {movieTitle}
             </h2>
             <p className="text-lg mb-3">
               <span className="font-semibold mr-1">Overview:</span>
-              {movieDetails.description}
+              {movieDescription}
             </p>
             <p className="mb-3">
               <span className="font-semibold mr-1">Date Released:</span>
--- src/app/tv/[id]/page.jsx
+++ src/app/tv/[id]/page.jsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import React from 'react'
+import { Banner } from "../../../../public/static/images/twitter_banner.png"
+import Head from 'next/head';
 
 const API_URL = process.env.BASE_URL;
 
@@ -16,11 +18,33 @@ export default async function TVDetailsPage({ params }) {
     const data = await getTVDetails(params.id);
     const tvDetails = data.data;
 
+    const tvTitle = tvDetails.title_en || tvDetails.title_original;
+    const tvDescription = tvDetails.description;
+    const tvImage = tvDetails.backdrop || tvDetails.image_url;
+
     return (
-        <div className="w-full">
+      <div className="w-full">
+        <Head>
+          <title>{movieTitle}</title>
+          <meta name="description" content={tvDescription} />
+
+          {/* Twitter metadata */}
+          <meta name="twitter:card" content={tvDetails.backdrop || Banner} />
+          <meta name="twitter:title" content={tvTitle} />
+          <meta name="twitter:description" content={tvDescription} />
+          <meta name="twitter:image" content={tvImage} />
+
+          {/* Facebook Open Graph metadata */}
+          <meta property="og:title" content={tvTitle} />
+          <meta property="og:description" content={tvDescription} />
+          <meta property="og:image" content={tvImage} />
+          <meta property="og:url" content={`${API_URL}/tv/${params.id}`} />
+          <meta property="og:type" content="website" />
+        </Head>
+
         <div className="p-4 md:pt-8 flex flex-col md:flex-row items-center content-center max-w-6xl mx-auto md:space-x-6">
           <Image
-            src={tvDetails.backdrop || tvDetails.image_url}
+            src={tvImage}
             width={500}
             height={300}
             className="rounded-lg"
@@ -33,11 +57,11 @@ export default async function TVDetailsPage({ params }) {
             alt="Movie poster" />
           <div className="p-2">
             <h2 className="text-lg mb-3 font-bold">
-              {tvDetails.title_en || tvDetails.title_original}
+              {tvTitle}
             </h2>
             <p className="text-lg mb-3">
               <span className="font-semibold mr-1">Overview:</span>
-              {tvDetails.description}
+              {tvDescription}
             </p>
             <p className="mb-3">
               <span className="font-semibold mr-1">Date Released:</span>

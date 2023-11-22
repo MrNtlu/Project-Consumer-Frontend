b1cb9a89c1dfcd63704714adcbf6137bee8582c9
Burak Fidan <97burakfidan97@gmail.com>
2023-11-22 18:20:21 +0300

    Minor bug fixed.

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -7,7 +7,7 @@ export default function Error({ error, reset }) {
   }, [error]);
   return (
     <div className="text-center mt-10">
-      <h1>Something went wrong</h1>
+      <h1>Something went wrong 😔</h1>
       <button className="hover:text-[#2196F3]" onClick={() => reset()}>Try Again</button>
     </div>
   );

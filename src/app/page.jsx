d0b44bb587b14b9b769f4400909da074168c6972
Burak Fidan <97burakfidan97@gmail.com>
2024-04-11 16:22:47 +0300

    Minor color change added.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
         <Pricing />
         <div className="fixed bottom-5 right-5 z-50">
           <a href="https://www.smartbio.me/cluv8s9xx0000ydw1i8uhjv7j">
-          <div className="w-16 h-16 inline-flex items-center justify-center rounded-full bg-[#2196F3] text-white mb-5">
+          <div className="w-16 h-16 inline-flex items-center justify-center rounded-full bg-[#2196F3] text-white mb-5 dark:hover:bg-gray-200 dark:hover:text-[#2196F3] hover:bg-[#212121] hover:text-[#2196F3]">
             <FaRobot size={28}/>
           </div>
           </a>
